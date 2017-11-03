1301b13e79923e224c5bfe98c310a0eac33bedbe
sp_ice <daisukeunoz1zi@gmail.com>
2017-11-03 16:19:41 +0900

    makewords

---
+++ app/Console/Commands/MakeWords.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\News;
+// use Illuminate\Validation\Validator;
+use Illuminate\Validation\Factory;
+
+class MakeWords extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'makewords';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $clc_news = $this->getNews();
+        print_r($clc_news);
+        exit;
+
+        foreach ($clc_news as $news) {
+            print_r($news);
+            exit;
+            
+            
+            try{
+                $rss = simplexml_load_file($url);
+                foreach ($rss->channel->item as $item) {
+                    $data = json_decode(json_encode($item),TRUE);
+
+                    $validator = app("validator");
+                    $v = $validator->make($data, [
+                        'link' => 'required|unique:news,url',
+                        'title' => 'required',
+                        'pubDate' => 'required',
+                    ]);
+                    if ($v->fails()) {
+                        $errors = $v->errors();
+                        foreach ($errors->all() as $message) {
+                            echo $message, "\n";
+                        }
+                        continue;
+                    }
+
+                    $news = $this->createNews($data,$src);
+                    echo 'OK:',$news->id,"\n";
+                }
+            } catch (Exception $e) {
+                echo 'ERR:',  $e->getMessage(), "\n";
+                continue;
+            }
+        }
+    }
+
+    /**
+     * Newsテーブル登録処理
+     *
+     * @param  array  $data
+     * @param  string  $src
+     * @return \App\News
+     */
+    protected function createNews(array $data, string $src)
+    {
+        return News::create([
+            'url' => $data['link'],
+            'title' => $data['title'],
+            'pub_date' => $data['pubDate'],
+            'src' => $src,
+            'flg_analyze' => 0,
+        ]);
+    }
+    
+    /**
+     * Newsテーブル抽出処理
+     *
+     * @return Collection
+     */
+    protected function getNews()
+    {
+        return News::where('flg_analyze', 0);
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\CollectNews::class,
+        \App\Console\Commands\MakeWords::class,
+        
     ];
 
     /**
