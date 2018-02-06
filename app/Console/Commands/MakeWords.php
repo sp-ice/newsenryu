1f886c6148014ef7f0f6a3f87d5b6cc140ef2c20
sp_ice <daisukeunoz1zi@gmail.com>
2018-02-07 01:15:47 +0900

    ５文字or７文字判定の実装

--- app/Console/Commands/MakeWords.php
+++ app/Console/Commands/MakeWords.php
@@ -5,8 +5,6 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\News;
 use App\Word;
-// use Illuminate\Validation\Validator;
-use Illuminate\Validation\Factory;
 
 class MakeWords extends Command
 {
@@ -44,49 +42,37 @@ class MakeWords extends Command
         $clc_news = $this->getNews();
         foreach ($clc_news as $news) {
             try{
-                $q = urlencode($news->title);
+                //形態素解析
+		$q = urlencode($news->title);
                 $url = "http://127.0.0.1/kuromoji?s=$q";
 		$ret_str = file_get_contents($url);
                 $ret_obj = json_decode($ret_str);
-                foreach($ret_obj as $keitaiso){
-print_r($keitaiso);
+		
+		//5文字or7文字判定
+		$pre_len_reading = 0;
+		$pre_reading="";
+		foreach($ret_obj as $keitaiso){
                     if(property_exists($keitaiso, 'reading')){
                         $len_reading = iconv_strlen($keitaiso->reading);
-print_r($len_reading);
                         if($len_reading == 5 || $len_reading == 7){
                             $word = $this->createWord($news->id, $keitaiso->reading);
                             echo 'OK:',$word->id,"\n";
-                        }
+                        }else if($len_reading + $pre_len_reading == 5 || $len_reading + $pre_len_reading == 7){
+			    $word = $this->createWord($news->id, $pre_reading.$keitaiso->reading);
+			}
+			$pre_reading = $keitaiso->reading;
+			$pre_len_reading = $len_reading;
                     }
                 }
-exit;
 
-                $rss = simplexml_load_file($url);
-                print_r($rss);
-exit;
-                foreach ($rss->channel->item as $item) {
-                    $data = json_decode(json_encode($item),TRUE);
-
-                    $validator = app("validator");
-                    $v = $validator->make($data, [
-                        'link' => 'required|unique:news,url',
-                        'title' => 'required',
-                        'pubDate' => 'required',
-                    ]);
-                    if ($v->fails()) {
-                        $errors = $v->errors();
-                        foreach ($errors->all() as $message) {
-                            echo $message, "\n";
-                        }
-                        continue;
-                    }
-
-                    $news = $this->createNews($data,$src);
-                    echo 'OK:',$news->id,"\n";
-                }
+		//newsを分析済みに更新
+		$news->flg_analyze=1;
+		$news->save();
+		
             } catch (Exception $e) {
                 echo 'ERR:',  $e->getMessage(), "\n";
-                continue;
+                var_dump($e->getTrace());
+		continue;
             }
         }
     }
@@ -107,24 +93,6 @@ exit;
         ]);
     }
 
-    /**
-     * Newsテーブル登録処理
-     *
-     * @param  array  $data
-     * @param  string  $src
-     * @return \App\News
-     */
-    protected function createNews(array $data, string $src)
-    {
-        return News::create([
-            'url' => $data['link'],
-            'title' => $data['title'],
-            'pub_date' => $data['pubDate'],
-            'src' => $src,
-            'flg_analyze' => 0,
-        ]);
-    }
-    
     /**
      * Newsテーブル抽出処理
      *
