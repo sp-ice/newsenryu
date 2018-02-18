b8e7d0d08da54fa7ac8bdaf372ffad43cef95fea
sp_ice <daisukeunoz1zi@gmail.com>
2018-02-18 23:49:47 +0900

    川柳コントローラー実装

--- app/Http/Controllers/SenryuController.php
+++ app/Http/Controllers/SenryuController.php
@@ -14,7 +14,7 @@ class SenryuController extends Controller
      */
     public function index()
     {
-        //
+        return response(Senryu::orderby('created_at', 'desc')->paginate());
     }
 
     /**
@@ -24,7 +24,7 @@ class SenryuController extends Controller
      */
     public function create()
     {
-        //
+        //no use
     }
 
     /**
@@ -35,7 +35,25 @@ class SenryuController extends Controller
      */
     public function store(Request $request)
     {
-        //
+//id           | bigint                         | not null default nextval('senryu_id_seq'::regclass)
+// user_id      | integer                        | not null
+// word_kami_id | integer                        | not null
+// word_naka_id | integer                        | not null
+// word_simo_id | integer                        | not null
+// good         | integer                        | not null
+// view         | integer                        | not null
+ //created_at   | timestamp(0) without time zone | 
+ //updated_at   | timestamp(0) without time zone |		
+        $senryu = new Senryu();
+        // todo: ログインユーザのidが入るようにする
+        $senryu->user_id = \App\User::query()->first()->id;
+        $senryu->word_kami_id = $request->input('word_kami_id');
+        $senryu->word_naka_id = $request->input('word_naka_id');
+        $senryu->word_simo_id = $request->input('word_simo_id');
+        $senryu->good = 0;
+        $senryu->view = 0;
+        $senryu->save();
+        return response($senryu, 201);
     }
 
     /**
@@ -46,7 +64,7 @@ class SenryuController extends Controller
      */
     public function show(Senryu $senryu)
     {
-        //
+        return response($senryu);
     }
 
     /**
@@ -57,7 +75,7 @@ class SenryuController extends Controller
      */
     public function edit(Senryu $senryu)
     {
-        //
+        //no use
     }
 
     /**
@@ -69,7 +87,24 @@ class SenryuController extends Controller
      */
     public function update(Request $request, Senryu $senryu)
     {
-        //
+	if($request->input('word_kami_id')){
+		$senryu->word_kami_id = $request->input('word_kami_id');
+	}
+	if($request->input('word_naka_id')){
+        	$senryu->word_naka_id = $request->input('word_naka_id');
+	}
+	if($request->input('word_simo_id')){
+       		$senryu->word_simo_id = $request->input('word_simo_id');
+	}
+	if($request->input('good')){
+        	$senryu->good = $request->input('good'));
+	}
+	if($request->input('view')){
+        	$senryu->view = $request->input('view'));
+	}
+        $senryu->save();
+        return response($senryu);
+ 
     }
 
     /**
@@ -80,6 +115,7 @@ class SenryuController extends Controller
      */
     public function destroy(Senryu $senryu)
     {
-        //
+    	$senryu->destroy();
+	return response('{}');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('/word', 'WordController', ['except' => ['create', 'edit', 'store', 'update', 'destroy']]);
+Route::resource('/senryu', 'SenryuController', ['except' => ['create', 'edit']]);
+
